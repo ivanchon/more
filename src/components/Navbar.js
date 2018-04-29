0305e654e523900d2b05133019daf76bec517ad8
ivanchon <ivan.chon@me.com>
2018-04-29 16:24:38 +0200

    added bio page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,29 +13,65 @@ const Navbar = () => (
             <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
           </figure>
         </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
+
+        <a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
         </a>
+      </div>
+      <div class="navbar-menu" id="navMenu">
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>
+          <Link className="navbar-item" to="/products">
+            Products
+          </Link>
+        </div>
+        <div className="navbar-end">
+          <a
+            className="navbar-item"
+            href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="icon">
+              <img src={github} alt="Github" />
+            </span>
+          </a>
+        </div>
       </div>
     </div>
   </nav>
 )
 
 export default Navbar
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
