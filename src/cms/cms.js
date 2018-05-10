f6832871c06b688efa61d6d385aef734e6f37c1a
ivanchon <ivan.chon@me.com>
2018-05-10 15:55:33 +0200

    created qa page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,10 +5,12 @@ import AlbumsPagePreview from './preview-templates/AlbumsPagePreview'
 import BioPagePreview from './preview-templates/BioPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import QAPagePreview from './preview-templates/QAPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('albums', AlbumsPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('bio', BioPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('qa', QAPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
